df19b1ac9ebbde105be0e7fee487cbca60a6da2a
Josh Cronin <jcronin@positive.co.uk>
2019-04-25 18:52:42 +0100

    Added gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
+const webserver = require('gulp-webserver');
 
-gulp.task('default', () => {
+gulp.task('build', () => {
     return gulp.src('./src/positive-lazy-load.js')
         .pipe(babel({
             presets: ['@babel/preset-env']
@@ -10,3 +11,17 @@ gulp.task('default', () => {
         .pipe(uglify())
         .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('serve', () => {
+    gulp.src('./')
+        .pipe(webserver({
+            host: '0.0.0.0',
+            port: 8000,
+            livereload: true,
+            directoryListing: true,
+            open: 'http://127.0.0.1:8000/index.html'
+        }));
+    gulp.watch('./src/**/*', gulp.series('build'));
+});
+
+gulp.task('default', gulp.series('build', 'serve'));
